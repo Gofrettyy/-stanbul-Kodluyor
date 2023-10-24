9f75c1f63a73c141e0ce9715d5b39ad4a2732571
Gofrettyy <140203411+Gofrettyy@users.noreply.github.com>
2023-10-24 22:51:35 +0300

    Add files via upload

---
+++ Methods/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Add();
+            //Add();
+            //Add();
+            //Add();
+            //var Return = Add2(20, 30);
+            //int number1 = 20;
+            //int number2 = 100;
+            //var Result = Add3(ref number1, number2);
+            //Console.WriteLine(Result);
+            //Console.WriteLine(number1);
+            Console.WriteLine(Multiply(2, 4));
+            Console.WriteLine(Multiply(2, 4, 5));
+            Console.WriteLine(Add4(1, 2, 3, 4, 5, 6, 7));
+            Console.ReadLine();
+        }
+        static void Add()
+        {
+            Console.WriteLine("Added");
+        }
+        // Methodları genel anlamda biz Don't Repeat Yourself! prensibi için yaparız.
+
+        static int Add2(int number1, int number2 = 30) //Buradaki = 30 number 2 ye yukarıda bir değer verilmezse number 2 için 30 değeri kullanılır.
+        {
+            return number1 + number2;
+
+
+        }
+        static int Add3(ref int number1, int number2)
+        {
+            number1 = 30;
+            return number1 + number2;
+
+        }
+        static int Multiply(int number1, int number2)
+        {
+            return number1 * number2;                                                             //Aynı methodu farklı parametrelerle kullanmaya Method Overloading denir. yandaki örnek  Method Overloading'e bir örnektir.
+        }
+
+        static int Multiply(int number1, int number2, int number3)
+        {
+            return number1 * number2 * number3;
+        }
+    }
+    static int Add4 (params int[] numbers)                                    // birden fazla parametreli method kullanmak ama Method overloading yapmak bizi yoracaksa onun yerine params kullanarak istediğimiz sonucu elde edebiliriz.
+    {
+        numbers.Sum();
+    }
+}
+    // void git bir yere bi şey yaz bir kayıt yap demek
+    //number1 ilk örnekte değer tip olduğu için değeri methodun içinde değiştirdiğimizde method dışındaki number1 değerini etkilemeyecektir.
+    // Referans tipler gibi bir yerde değerini değiştirdiğimizde her yerde değişmesini işstersen Ref keywordu kullanarak değişkenin davranış biçimini yani tipini değiştirebiliriz.
+    // refin bir alternatifi de outtur. aynı şekilde kullanılır. fakat outta method dışındada number1a bir değer atamalıydın ama outta atamasanda olur.
---
+++ TypesAndVariables/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace TypesAndVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int number1 = 2147483641;
+            long number2 = 214547483648;
+            short number3 = 32767;
+            byte number4 = 255;
+            bool condition = false;
+            char character = 'A';
+            double number5 = 10.4;
+            decimal number6 = 10.4m;
+            var number7 = 10;
+            number7 = 'A'; //65 olarak gözükecek çünkü artık var ile number 7 ye int bir değer atadık. çift üst virgüller string atamaya çalışsaydık number 7 ye string atayamazdık çünkü number 7 bir int.
+
+            Console.WriteLine("Number1 is {0}", number1);
+            Console.WriteLine("Number2 is {0}", number2);
+            Console.WriteLine("Number3 is {0}", number3);
+            Console.WriteLine("Number4 is {0}", number4);
+            Console.WriteLine("Number5 is {0}", number5);
+            Console.WriteLine("Number7 is {0}", number7);
+            Console.WriteLine("Character is : {0}", (int)character);
+            Console.WriteLine(days.Friday);
+            Console.ReadLine();
+
+
+        }
+
+
+    }
+
+    enum days
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+}
+
+
+
+//number1 ise değişkendir 
+/*Value Types (Değer Tipleri)  Bütün veri tiplerinin bir sınırı vardır.
+ * Interger(int)= Tamsayı sınırı = 2147483647 32 bitlik yer kaplar.
+ * long (long) = 62 bitlik yer kaplar intergerın iki katı kadar bellekte yer kaplar. longun sınırı tam 19 karakterden oluşur. -9223372036854775808 ve 9223372036854775807 sınırlarıdır.
+ * short (short) = 16 bitlik yer kaplar.
+ * byte (byte) = 0 la 255 arası sayıları tutar.
+ * boolean (bool)= Genellikle if gibi karşılaştırma bloklarında kullanırız. o yüzden condition kullanırız.
+ * char character = integer bir veritipine dönüştürülebilir.
+ * double = ondalıklı sayıları ifade ederken kullanılır fakat integer gibi bir değerde atayabilirsin. fakat double bir değeri integera yalın haliyle atayamayız tip dönüşümü gerekir.Virgülden sonra 15 16 değer tutabilir.
+ * decimal =integer için long ne ise double içinde decimal odur diyebiliriz. Hassiyet gösteren işlemler için kullanabilir. virgülden sonra 28 29 değer tutar.10.4m ile yazarken sadece 10 yazdığımızda m eklememize gerek yok.
+ * Enum = En temel amaç matching string denilen değerler tutmak yerine bir enum sabiti vasıtasıyla çoklu string değerleri string ile yazıp bir daha bir daha yazmaktansa 
+ * tek bir parantezde bütün örnekleri görmemizi sağlayan değer tipidir.Karşılaştırma yapacağımız zaman kullanılır.
+ * var keyword değişken değildir fakat değişken tutmak için kullanılır.
+ */
