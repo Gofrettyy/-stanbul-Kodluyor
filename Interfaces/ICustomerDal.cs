097a9c56db1900070d48af219a521ff5986ae63a
Nazife Aliçavuş <140203411+Gofrettyy@users.noreply.github.com>
2023-10-27 01:39:53 +0300

    Add files via upload

---
+++ Interfaces/ICustomerDal.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Interfaces
+{
+    interface ICustomerDal //veritabanı işlemlerini bu dal sınıfında yaparız. ister sql yada oracleda yap aynı interface i kullanırız farklı kodlar kullanırız ama
+    {
+        void Add();
+        void Update();
+        void Delete();
+    }
+    class SqlServerCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Sql added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Sql deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine(" Sql updated");
+        }
+    }
+    class OracleCustomerDal : ICustomerDal
+    {
+        public void Add()
+        {
+            Console.WriteLine("Oracle added");
+        }
+
+        public void Delete()
+        {
+            Console.WriteLine("Oracle deleted");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine(" Oracle updated");
+        }
+
+    }
+    class CustomerManager
+    {
+        public void Add(ICustomerDal customerDal)
+        {
+            customerDal.Add();
+        }
+    }
+}
+
+
+
---
+++ Interfaces/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args) // Amaç temel nesne oluşturup bütün nesneleri ondan implemente etmektir. İnterfaceler servis implamentasyonu yaparken veya katmanlar arası geçiş yaparken kullanırız.
+        {
+            //InterfacesIntro();        // Bir interface hiçbir zaman newlenemez. kendi başına newlemek mümkün değildir.Soyut nesneler n
+            //Demo();
+
+            ICustomerDal[] customerDals = new ICustomerDal[2]  // interfaceler gerçekten gerçek hayatta bu şekilde kullanılıyor. Böyle Arrayli kullanımın avantajı yeni bir veri tabanı eklediğimizde kolaylıkla desteklenebiliyor olması.
+            {
+            new SqlServerCustomerDal(),
+            new OracleCustomerDal()
+            };
+            foreach (var customerDal in customerDals)
+            {
+                customerDal.Add();
+            }
+
+            Console.ReadLine();
+
+        }
+
+        private static void Demo()
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(new SqlServerCustomerDal());
+        }
+
+        private static void InterfacesIntro()
+        {
+            PersonManager manager = new PersonManager();
+            manager.Add(new Customer());
+            manager.Add(new Customer { Id = 1, FirstName = "Engin", LastName = "Demiroğ", Address = "Ankara" });
+            Student student = new Student
+            {
+                Id = 1,
+                FirstName = "Derin",
+                LastName = "Demiroğ",
+                Department = "Computer Sciences"
+            };
+            manager.Add(student);
+        }
+
+        interface IPerson  // SOYUT NESNELER tek başına hiç bir anlam ifade etmez 
+        {
+            int Id { get; set;  }       // sen bu özellikleri tanımladıysan bu özellikleri implemente eden classlardada yazmak zorundasın.
+            string FirstName{ get; set; }
+            string LastName { get; set; }
+        }
+        class Customer: IPerson  // SOMUT NESNELER
+        {
+           public int Id { get; set; }     
+           public  string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address{ get; set; }
+        }
+
+        class Student : IPerson
+        {
+            public int Id { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Department{ get; set; }
+        }
+        class PersonManager
+        {
+            public void Add(IPerson person)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+        }
+    }
+}
