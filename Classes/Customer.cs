328c0504ee27127842bcbcf4a281218af59419a4
Nazife Aliçavuş <140203411+Gofrettyy@users.noreply.github.com>
2023-10-27 00:31:05 +0300

    Add files via upload

---
+++ Classes/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Classes
+{  //PROPERTYS
+  class Customer   
+	{
+        // Field (Alan) tanımlamak. public string FirstName;
+		public int Id { get; set; }  // Bir müşterinin özelliklerini tutmak için kullandığımız nesnedir. Genellikle veri tabanlarında ki kolonların karşılığı olarak biz burda bir class için tutabiliriz.
+        // Mesela biz first namede bir değişiklik yapmak isteyelim. başına mr yazmak gibi kişi set edebilir fakat get ederken olay değişkenlik gösterir bu yüzden bloğu daha açık yazmamız gerekir.
+        private string _firstName;
+        public string FirstName
+        {
+            get { return "Mr." + _firstName; }
+            set { _firstName = value; }
+        }   //bazı programcılar bunu böyle tanımlamak yerine yukarıdaki gibi tanımlarlar ama biz eğer classın özelliklerini anlatacaksak field değilde property olarak tanımlamamız gerekir
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+    // niye property olarak tanımlıyoruz get ve set ne işe yarıyor ? ENCAPSULATION adı altında konuşulacak.
+
+}
+
---
+++ Classes/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager(); //classı kullanabilmek için böyle örnek oluştururuz.
+            customerManager.Add();
+            customerManager.Update();
+            ProductManager productManager = new ProductManager(); //classı kullanabilmek için böyle örnek oluştururuz.
+            productManager.Add();
+            productManager.Update();
+
+            Customer customer = new Customer();
+            customer.City = "Ankara";
+            customer.Id = 1;
+            customer.FirstName = "Engin";  //burada bir classın propertysini eşitlediğiniz zaman aslında set etmiş oluyorsunuz yani classlarda yazdığımız set çalışmış oluyor.
+            customer.LastName = "Demiroğ";
+            Customer customer2 = new Customer
+            {
+                Id = 2, City= "İstanbul", FirstName = "Derin", LastName="Demiroğ"
+
+            };
+
+            Console.WriteLine(customer2.FirstName);
+
+            Console.ReadLine();
+
+
+        }
+    }
+    class CustomerManager    // Bir class oluştururken kelimelerin ilk harfleri büyük oluşturulur. sonrasında örneği oluşturulduktan sonra camel case e geri dönülür.
+    {
+        public void Add()
+        {
+            Console.WriteLine("Customer Added");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Customer Updated");
+        }
+    }
+    class ProductManager {  
+        public void Add()
+        {
+            Console.WriteLine("Product Added");
+        }
+        public void Update()
+        {
+            Console.WriteLine("Product  Updated");
+        }
+    }
+}
+
+// OOP'nin en temel unsurudur. 
